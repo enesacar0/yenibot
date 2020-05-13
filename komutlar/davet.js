4295aaf71ec78cb0f1db96daac9ab8581d92c153
Glitch (efesarenabots) <none>
2020-05-13 15:59:41 +0000

    enesacar

---
+++ komutlar/davet.js
@@ -0,0 +1,29 @@
+const Discord = require('discord.js');
+
+let botid = ('670672712498151454') //bu yere botun id'sini yapıştırın.
+//eğer botunuz dbl(discord bot list) de yoksa Bota Oy Ver (Vote) olmucaktır.
+
+exports.run = (client, message, args) => {
+    const embed = new Discord.RichEmbed()
+    .setColor("RANDOM")
+    .setAuthor(`${client.user.username} İletişim Bilgileri`)
+    .addField('BİZ KİMİZ?', 'Discord kullanıcıların sunucularını daha iyi hale getirmek için çabalayan bir ekibiz.')//ne kadar yetkili komutunuz varsa o kadar .addField('prefix+komut', 'açıklama/kullanım amacı') koyun
+    .addField(`» Linkler`, `[Bot Davet Linki](https://discord.com/oauth2/authorize?client_id=670672712498151454&scope=bot&permissions=0s
+) **|** [Destek Sunucusu](https://discord.gg/jyPzXRV) **|**  **|** [Web Sitesi ( Yakında )]()`)//websiteniz yoksa  **|** [Web Sitesi]() yeri silebilirsiniz
+    message.channel.sendEmbed(embed);
+
+};
+
+exports.conf = {
+  enabled: true,
+  guildOnly: false,
+  aliases: [],
+  permLevel: 0,
+};
+
+exports.help = {
+  name: 'bizkimiz',
+  description: 'bot hakkında bilgi',
+  usage: 'davetet'
+};
+   
